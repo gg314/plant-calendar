6dc42f5a0c1aab21e02c32f779f5323b39fff7bb
gg314 <54562746+gg314@users.noreply.github.com>
2020-04-17 08:59:33 -0600

    Add some cool icons; basic ZIP support

--- src/PlantCalendar.elm
+++ src/PlantCalendar.elm
@@ -3,6 +3,7 @@ module PlantCalendar exposing (Model, Msg(..), init, main, update, subscriptions
 import Browser
 import Browser.Dom
 import Browser.Events
+import Http
 import Html exposing (Html, div, span, text, h2, blockquote, ul, li, a, main_, textarea, button, strong, br, p, input, form, label)
 import Html.Attributes exposing (class, id, placeholder, value, href, target, type_, for, checked)
 import Html.Events exposing (onClick, onInput, onFocus, onBlur)
@@ -11,9 +12,9 @@ import Svg.Attributes exposing (style, x, y, x1, x2, y1, y2, stroke, fill, width
 import Array exposing (Array, fromList, get, slice)
 import Set exposing (fromList, toList)
 import Task
+import Json.Decode
 
 -- MAIN
-
 main : Program () Model Msg
 main =
     Browser.element
@@ -25,16 +26,29 @@ main =
 
 
 -- MODEL
+type HTTPStatus
+  = Success String
+  | Failure
+  | Loading
+
+
+type alias Zipcode =
+  { zone : String
+  , lat : String
+  , lon : String
+  }
 
 type alias Plant = 
   { name : String 
+  , category : String
   , selected : Bool
   }
 
 
 type alias Model =
-    { zipcode : Int
-    , phz : Int
+    { zipcode : Zipcode
+    , zipcodetext : String
+    , phz : HTTPStatus
     , plants : List ( Plant )
     , filter : String
     }
@@ -43,13 +57,10 @@ type alias Model =
 init : () -> (Model, Cmd Msg)
 init _ =
     (Model
-      11234
-      5
-      [ {name = "Carrots", selected = True}
-      , {name = "Beans", selected = True}
-      , {name = "Potatos", selected = False}
-      , {name = "Arugula", selected = True}
-      , {name = "Spinach", selected = False}]
+      (Zipcode "-1" "-1" "-1")
+      ""
+      Failure
+      plantData
       ""
     , Cmd.none)
 
@@ -58,17 +69,45 @@ init _ =
 
 type Msg
     = SetZipcode
+    | SetZipcodeText String
     | TogglePlant Plant
     | SetFilter String
+    | GotZipcode String (Result Http.Error Zipcode)
+
+
+jsonDecoder : Json.Decode.Decoder Zipcode
+jsonDecoder =
+    Json.Decode.map3
+        Zipcode
+            (Json.Decode.field "zone" Json.Decode.string)
+            (Json.Decode.field "rangemin" Json.Decode.string)
+            (Json.Decode.field "rangemax" Json.Decode.string)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg)
 update msg model =
     case msg of
       SetZipcode ->
-          ( { model | zipcode = 12345 }
-          , Cmd.none)
-          
+        let
+            searchZip = model.zipcodetext
+        in
+            ( { model | phz = Loading, zipcodetext = "" }
+            , Http.get { url = "https://c0bra.api.stdlib.com/zipcode-to-hardiness-zone/?zipcode="++ searchZip,
+                         expect = Http.expectJson (GotZipcode searchZip) jsonDecoder }
+            )
+
+      SetZipcodeText str ->
+        ( { model | zipcodetext = str }, Cmd.none)
+      
+      GotZipcode searchZip result ->
+        case result of
+            Ok z ->
+              ({ model | zipcode = z, phz = Success z.zone, zipcodetext = ""}, Cmd.none)
+
+            Err _ ->
+              ({ model | zipcode = Zipcode "-1" "-1" "-1", phz = Failure}, Cmd.none)
+        
+
       TogglePlant plant ->
         let
           newPlants = List.map (togglePlant plant) model.plants
@@ -96,7 +135,7 @@ getPlants : Int -> List (Plant) -> List (Html Msg)
 getPlants index plants =
   case plants of
     p::ps ->
-      div [] [ input [ type_ "checkbox", id ("p"++ (String.fromInt index)), checked p.selected, onClick (TogglePlant p) ] [], label [ for ("p"++ (String.fromInt index)) ] [ text p.name ] ] :: getPlants (index + 1) ps
+      div [] [ input [ type_ "checkbox", id ("p"++ (String.fromInt index)), checked p.selected, onClick (TogglePlant p) ] [], label [ class p.category, for ("p"++ (String.fromInt index)) ] [ text p.name ] ] :: getPlants (index + 1) ps
     _ -> []
 
 drawSVG : List (Plant) -> Html Msg
@@ -153,15 +192,21 @@ drawRow index plant =
         , Svg.line [ y1 (String.fromInt y0), y2 (String.fromInt y0), x1 "99.9%", x2 "0%" ] []
         , Svg.rect [ x "32%", y (String.fromInt (y0-18)), width "20%", height "14", stroke "#517f6c", fill "rgba(56, 165, 116, .7)"] []
         , Svg.rect [ x "52%", y (String.fromInt (y0+4)), width "3%", height "14", stroke "#7c6650", fill "rgba(211, 122, 41, .7)"] []
-        ]
-    {--
-            <rect x="32%" y="62" width="20%" height="14" stroke="#517f6c" fill="rgba(56, 165, 116, .7)" rx="0"></rect>
-            <rect x="52%" y="84" width="3%" height="14" stroke="#7c6650" fill="rgba(211, 122, 41, .7)" rx="0"></rect>
-       --}     
+        ] 
 
 plantNameContains : String -> Plant -> Bool
 plantNameContains search plant =
     String.contains search (String.toLower (.name plant))
+
+getPHZ : HTTPStatus -> String
+getPHZ status =
+    case status of
+        Success good ->
+          good
+        Loading ->
+          "Loading..."
+        Failure ->
+          "Please set ZIP Code to find Plant Hardiness Zone. The default (Zone 5) will be used until you do."
 
 view : Model -> Html Msg
 view model =
@@ -169,14 +214,14 @@ view model =
     sidebarPlants = List.filter (plantNameContains (String.toLower model.filter)) model.plants
     selectedPlants = List.filter .selected model.plants
   in
-    form []
+    div []
     [ div [ class "header" ] [ text "plant-calendar" ]
     , div [ class "container" ]
       [ div [ class "sidebar" ]
         [ div [ class "sidebar__top" ]
           [ div [ class "location" ]
-            [ input [ type_ "text", id "location", placeholder "Zip code" ] []
-            , button [] [ text "Set" ]
+            [ input [ type_ "text", id "location", placeholder "ZIP Code", onInput SetZipcodeText, value model.zipcodetext ] []
+            , button [ onClick SetZipcode ] [ text ("Set") ]
             ]
           , div [ class "search" ]
             [ input [ type_ "search", id "filter", placeholder "Filter", onInput SetFilter, value model.filter ] []
@@ -184,9 +229,12 @@ view model =
             ]
           ]
         , div [ class "sidebar__bottom" ]
-            [ div [ class "sidebar__bottom" ] (getPlants 0 sidebarPlants) ]
+            [ div [ class "crop__list" ] (getPlants 0 sidebarPlants) ]
         ]
-      , main_ [] [ drawSVG selectedPlants ]
+      , main_ []
+        [ text (getPHZ model.phz)
+        , drawSVG selectedPlants
+        ]
       ]
     ]
 {--
@@ -267,3 +315,147 @@ view model =
   </div>
   </form>
   --}
+  
+plantData : List (Plant)
+plantData = 
+    [ {name = "Anemone", category = "Flowers", selected = False}
+    , {name = "Anise Hyssop", category = "Flowers", selected = False}
+    , {name = "Asters", category = "Flowers", selected = False}
+    , {name = "Astilbe", category = "Flowers", selected = False}
+    , {name = "Baby's Breath", category = "Flowers", selected = False}
+    , {name = "Bee Balm", category = "Flowers", selected = False}
+    , {name = "Black-Eyed Susans", category = "Flowers", selected = False}
+    , {name = "Butterfly Bush", category = "Flowers", selected = False}
+    , {name = "Borage (star flower)", category = "Flowers", selected = False}
+    , {name = "Calendula", category = "Flowers", selected = False}
+    , {name = "Cannas", category = "Flowers", selected = False}
+    , {name = "Carnations", category = "Flowers", selected = False}
+    , {name = "Celosia", category = "Flowers", selected = False}
+    , {name = "Chrysanthemum", category = "Flowers", selected = False}
+    , {name = "Clematis", category = "Flowers", selected = False}
+    , {name = "Columbine", category = "Flowers", selected = False}
+    , {name = "Coneflowers", category = "Flowers", selected = False}
+    , {name = "Coreopsis", category = "Flowers", selected = False}
+    , {name = "Cosmos", category = "Flowers", selected = False}
+    , {name = "Crocuses", category = "Flowers", selected = False}
+    , {name = "Daffodils", category = "Flowers", selected = False}
+    , {name = "Dahlias", category = "Flowers", selected = False}
+    , {name = "Daisies", category = "Flowers", selected = False}
+    , {name = "Daylilies", category = "Flowers", selected = False}
+    , {name = "Delphiniums", category = "Flowers", selected = False}
+    , {name = "Echinacea", category = "Flowers", selected = False}
+    , {name = "Gardenias", category = "Flowers", selected = False}
+    , {name = "Geraniums", category = "Flowers", selected = False}
+    , {name = "Gladiolus", category = "Flowers", selected = False}
+    , {name = "Hibiscus", category = "Flowers", selected = False}
+    , {name = "Hollyhock", category = "Flowers", selected = False}
+    , {name = "Honeysuckle", category = "Flowers", selected = False}
+    , {name = "Hyacinth", category = "Flowers", selected = False}
+    , {name = "Hydrangea", category = "Flowers", selected = False}
+    , {name = "Impatiens", category = "Flowers", selected = False}
+    , {name = "Irises", category = "Flowers", selected = False}
+    , {name = "Jasmine", category = "Flowers", selected = False}
+    , {name = "Lilies", category = "Flowers", selected = False}
+    , {name = "Marigolds", category = "Flowers", selected = False}
+    , {name = "Morning Glories", category = "Flowers", selected = False}
+    , {name = "Nasturtium", category = "Flowers", selected = False}
+    , {name = "Pansies", category = "Flowers", selected = False}
+    , {name = "Peonies", category = "Flowers", selected = False}
+    , {name = "Petunias", category = "Flowers", selected = False}
+    , {name = "Phlox", category = "Flowers", selected = False}
+    , {name = "Poppy", category = "Flowers", selected = False}
+    , {name = "Roses", category = "Flowers", selected = False}
+    , {name = "Salvia", category = "Flowers", selected = False}
+    , {name = "Sedum", category = "Flowers", selected = False}
+    , {name = "Shasta Daisies", category = "Flowers", selected = False}
+    , {name = "Strawflowers", category = "Flowers", selected = False}
+    , {name = "Sunflowers", category = "Flowers", selected = False}
+    , {name = "Sweet Peas", category = "Flowers", selected = False}
+    , {name = "Tuberose", category = "Flowers", selected = False}
+    , {name = "Tulips", category = "Flowers", selected = False}
+    , {name = "Verbenas", category = "Flowers", selected = False}
+    , {name = "Veronica (Speedwell)", category = "Flowers", selected = False}
+    , {name = "Viola", category = "Flowers", selected = False}
+    , {name = "Yarrow", category = "Flowers", selected = False}
+    , {name = "Zinnias", category = "Flowers", selected = False}
+    , {name = "Artichoke", category = "Vegetables", selected = False}
+    , {name = "Arugula", category = "Vegetables", selected = False}
+    , {name = "Asparagus", category = "Vegetables", selected = False}
+    , {name = "Beans", category = "Vegetables", selected = False}
+    , {name = "Beets", category = "Vegetables", selected = False}
+    , {name = "Bell Peppers", category = "Vegetables", selected = False}
+    , {name = "Broccoli", category = "Vegetables", selected = False}
+    , {name = "Brussels Sprouts", category = "Vegetables", selected = False}
+    , {name = "Cabbage", category = "Vegetables", selected = False}
+    , {name = "Carrots", category = "Vegetables", selected = False}
+    , {name = "Cauliflower", category = "Vegetables", selected = False}
+    , {name = "Celery", category = "Vegetables", selected = False}
+    , {name = "Collards", category = "Vegetables", selected = False}
+    , {name = "Corn", category = "Vegetables", selected = False}
+    , {name = "Cucumbers", category = "Vegetables", selected = False}
+    , {name = "Edamame", category = "Vegetables", selected = False}
+    , {name = "Eggplants", category = "Vegetables", selected = False}
+    , {name = "Endive", category = "Vegetables", selected = False}
+    , {name = "Fava Beans", category = "Vegetables", selected = False}
+    , {name = "Garlic", category = "Vegetables", selected = False}
+    , {name = "Gourds", category = "Vegetables", selected = False}
+    , {name = "Green Beans", category = "Vegetables", selected = False}
+    , {name = "Horseradish", category = "Vegetables", selected = False}
+    , {name = "Kale", category = "Vegetables", selected = False}
+    , {name = "Leeks", category = "Vegetables", selected = False}
+    , {name = "Lettuce", category = "Vegetables", selected = False}
+    , {name = "Okra", category = "Vegetables", selected = False}
+    , {name = "Onions", category = "Vegetables", selected = False}
+    , {name = "Parsnips", category = "Vegetables", selected = False}
+    , {name = "Peas", category = "Vegetables", selected = False}
+    , {name = "Potatoes", category = "Vegetables", selected = False}
+    , {name = "Pumpkins", category = "Vegetables", selected = False}
+    , {name = "Radishes", category = "Vegetables", selected = False}
+    , {name = "Rhubarb", category = "Vegetables", selected = False}
+    , {name = "Rutabagas", category = "Vegetables", selected = False}
+    , {name = "Shallots", category = "Vegetables", selected = False}
+    , {name = "Snap Peas", category = "Vegetables", selected = False}
+    , {name = "Soybean", category = "Vegetables", selected = False}
+    , {name = "Spinach", category = "Vegetables", selected = False}
+    , {name = "Squash", category = "Vegetables", selected = False}
+    , {name = "Sweet Potatoes", category = "Vegetables", selected = False}
+    , {name = "Swiss Chard", category = "Vegetables", selected = False}
+    , {name = "Tomatoes", category = "Vegetables", selected = False}
+    , {name = "Turnips", category = "Vegetables", selected = False}
+    , {name = "Zucchini", category = "Vegetables", selected = False}
+    , {name = "Basil", category = "Herbs", selected = False}
+    , {name = "Catnip", category = "Herbs", selected = False}
+    , {name = "Chives", category = "Herbs", selected = False}
+    , {name = "Coriander/Cilantro", category = "Herbs", selected = False}
+    , {name = "Dill", category = "Herbs", selected = False}
+    , {name = "Fennel", category = "Herbs", selected = False}
+    , {name = "Lavender", category = "Herbs", selected = False}
+    , {name = "Lemon Grass", category = "Herbs", selected = False}
+    , {name = "Marjoram", category = "Herbs", selected = False}
+    , {name = "Mint", category = "Herbs", selected = False}
+    , {name = "Mustard", category = "Herbs", selected = False}
+    , {name = "Oregano", category = "Herbs", selected = False}
+    , {name = "Parsley", category = "Herbs", selected = False}
+    , {name = "Rosemary", category = "Herbs", selected = False}
+    , {name = "Sage", category = "Herbs", selected = False}
+    , {name = "Scallion", category = "Herbs", selected = False}
+    , {name = "Stevia", category = "Herbs", selected = False}
+    , {name = "Tarragon", category = "Herbs", selected = False}
+    , {name = "Thyme", category = "Herbs", selected = False}
+    , {name = "Apples", category = "Fruits", selected = False}
+    , {name = "Blackberries", category = "Fruits", selected = False}
+    , {name = "Blueberries", category = "Fruits", selected = False}
+    , {name = "Cantaloupes", category = "Fruits", selected = False}
+    , {name = "Currant", category = "Fruits", selected = False}
+    , {name = "Cherries", category = "Fruits", selected = False}
+    , {name = "Figs", category = "Fruits", selected = False}
+    , {name = "Grapes", category = "Fruits", selected = False}
+    , {name = "Honeydew", category = "Fruits", selected = False}
+    , {name = "Lemons & Oranges", category = "Fruits", selected = False}
+    , {name = "Peaches", category = "Fruits", selected = False}
+    , {name = "Pears", category = "Fruits", selected = False}
+    , {name = "Plums", category = "Fruits", selected = False}
+    , {name = "Raspberries", category = "Fruits", selected = False}
+    , {name = "Strawberries", category = "Fruits", selected = False}
+    , {name = "Watermelon", category = "Fruits", selected = False}
+    ]
